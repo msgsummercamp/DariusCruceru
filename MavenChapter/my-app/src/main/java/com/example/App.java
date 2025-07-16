1722e2785020cc4a17bbc6ab7464c5fcc04fa97c
dariuscruceru21 <dariuscruceru@gmail.com>
2025-07-16 15:46:50 +0300

    Corrected Typo

--- MavenChapter/my-app/src/main/java/com/example/App.java
+++ MavenChapter/my-app/src/main/java/com/example/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Enviorment " + System.getProperty("env"));
+        System.out.println( "Enviornment " + System.getProperty("env"));
     }
 }
