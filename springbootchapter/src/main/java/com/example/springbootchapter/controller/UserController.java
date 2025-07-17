61422425a4c9d164bd7ce29106e3704c82e77d18
dariuscruceru21 <dariuscruceru@gmail.com>
2025-07-17 11:32:07 +0300

    added logs for the Requests

--- springbootchapter/src/main/java/com/example/springbootchapter/controller/UserController.java
+++ springbootchapter/src/main/java/com/example/springbootchapter/controller/UserController.java
@@ -2,6 +2,9 @@ package com.example.springbootchapter.controller;
 
 import com.example.springbootchapter.model.User;
 import com.example.springbootchapter.service.UserService;
+import com.example.springbootchapter.service.UserServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,6 +16,8 @@ import java.util.concurrent.atomic.AtomicLong;
 public class UserController {
     private final UserService userService;
 
+    private static final Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
+
     @Autowired
     public UserController(UserService userService) {
         this.userService = userService;
@@ -20,26 +25,31 @@ public class UserController {
 
     @GetMapping
     public List<User> getUsers() {
+        logger.info("Fetches all users");
         return userService.getAllUsers();
     }
 
     @PostMapping
     public void addUser(@RequestBody User user) {
+        logger.info("Adding user: {}", user);
         userService.addUser(user);
     }
 
     @DeleteMapping("/{id}")
     public void deleteUser(@PathVariable Long id) {
+        logger.info("Deleting user with id: {}", id);
         userService.deleteUser(id);
     }
 
     @PutMapping
     public void updateUser(@RequestBody User user) {
+        logger.info("Updating user: {}", user);
         userService.updateUser(user);
     }
 
     @GetMapping("/{id}")
     public User getUserById(@PathVariable Long id) {
+        logger.info("Fetching user with id: {}", id);
         return userService.getUserById(id);
     }
 
