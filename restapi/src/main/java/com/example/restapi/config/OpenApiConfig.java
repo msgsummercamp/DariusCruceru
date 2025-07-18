1f3a84fad39e97e2f1f7c5129ffa8926f8c59083
dariuscruceru21 <dariuscruceru@gmail.com>
2025-07-18 14:11:53 +0300

    finished Rest Chapter

---
+++ restapi/src/main/java/com/example/restapi/config/OpenApiConfig.java
@@ -0,0 +1,18 @@
+package com.example.restapi.config;
+
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Rest Api Management")
+                        .version("1.0")
+                        .description("Rest API for managing users"));
+    }
+}
--- restapi/src/main/java/com/example/restapi/model/User.java
+++ restapi/src/main/java/com/example/restapi/model/User.java
@@ -1,9 +1,9 @@
 package com.example.restapi.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -13,6 +13,7 @@ import lombok.Setter;
 
 @Entity
 @Getter
+@Schema(description = "User entity")
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
