58994e808016059750821d2e1620e8cbaaacade4
dariuscruceru21 <dariuscruceru@gmail.com>
2025-07-24 09:56:58 +0300

    finished RxJS Chapter PR Review

--- RxJSChapter/src/app/dogComponent/dog.components.ts
+++ RxJSChapter/src/app/dogComponent/dog.components.ts
@@ -1,4 +1,4 @@
-import {Component, signal} from '@angular/core';
+import {Component, inject, signal} from '@angular/core';
 import {DogService} from './dog.service';
 import {CommonModule} from '@angular/common';
 import {MatButton} from '@angular/material/button';
@@ -6,15 +6,18 @@ import {MatButton} from '@angular/material/button';
 @Component({
   selector: 'app-dog',
   templateUrl: './dog.component.html',
-  standalone:true,
   imports: [CommonModule, MatButton],
 })
 export class DogComponent{
   dogImage = signal<string | null>(null);
+  private dogService = inject(DogService)
 
-  constructor(private dogService : DogService){}
 
   getDogImage(){
     this.dogService.fetchDogImage().subscribe(url => this.dogImage.set(url));
+  }
+
+  get DogImageValue() {
+    return this.dogImage();
   }
 }
--- RxJSChapter/src/app/dogComponent/dog.service.ts
+++ RxJSChapter/src/app/dogComponent/dog.service.ts
@@ -1,17 +1,31 @@
-import {Injectable} from '@angular/core';
+import {inject, Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {map, Observable} from 'rxjs';
+import {map, first , Observable, pipe} from 'rxjs';
+
+type DogApiResponse = {
+  message: string;
+  status: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
-export class DogService{
-  constructor(private http :HttpClient){}
+export class DogService {
+  private http = inject(HttpClient);
+  private lastImage: string | null = null;
 
-  fetchDogImage(): Observable<string>{
-    return this.http.get<{message: string}>('https://dog.ceo/api/breeds/image/random')
+  fetchDogImage(): Observable<string> {
+    return this.http.get<DogApiResponse>('https://dog.ceo/api/breeds/image/random')
       .pipe(
-        map(response => response.message)
+        first(),
+        map(response => {
+          this.lastImage = response.message;
+          return response.message;
+        }),
       );
+  }
+
+  get lastDogImage(): string | null {
+    return this.lastImage;
   }
 }
