aa6d5d5fefe30e98f8acdeb3d9a4db12cb12116b
dariuscruceru21 <dariuscruceru@gmail.com>
2025-07-24 14:35:43 +0300

    finished Routing Feature

--- components-chapter/src/app/app.routes.ts
+++ components-chapter/src/app/app.routes.ts
@@ -1,3 +1,18 @@
-import { Routes } from '@angular/router';
+import {provideRouter, Routes} from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {LoginComponent} from "./login-component/login-component.component";
+import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
+import {bootstrapApplication} from "@angular/platform-browser";
+import {AppComponent} from "./app.component";
 
-export const routes: Routes = [];
+export const routes:Routes = [
+    {path: '', redirectTo: '/home', pathMatch: 'full'},
+    {path: 'home', component: HomeComponent},
+    {path: 'login',
+    loadComponent: () => import('./login-component/login-component.component').then(m => m.LoginComponent)},
+    {path: '**', component: PageNotFoundComponent}
+]
+
+bootstrapApplication(AppComponent, {
+    providers: [provideRouter(routes)],
+});
--- components-chapter/src/app/tool-bar/tool-bar.component.ts
+++ components-chapter/src/app/tool-bar/tool-bar.component.ts
@@ -1,14 +1,15 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {MatToolbar, MatToolbarRow} from '@angular/material/toolbar';
 import {ButtonComponent} from '../button/button.component';
 import {MatIcon} from '@angular/material/icon';
-import {MatMiniFabButton} from '@angular/material/button';
+import {MatButton, MatMiniFabButton} from '@angular/material/button';
+import {RouterLink, RouterLinkActive} from "@angular/router";
 
 @Component({
-  selector: 'app-tool-bar',
-  imports: [MatToolbar, ButtonComponent, MatToolbarRow, MatMiniFabButton, MatIcon],
-  templateUrl: './tool-bar.component.html',
-  styleUrl: './tool-bar.component.scss'
+    selector: 'app-tool-bar',
+    imports: [MatToolbar,  MatToolbarRow, MatMiniFabButton, MatIcon, RouterLink, RouterLinkActive, MatButton],
+    templateUrl: './tool-bar.component.html',
+    styleUrl: './tool-bar.component.scss',
 })
 export class ToolBarComponent {
 
